ec25c3465e79d1806604c08a1755d3541c91d64a
vivek kumar <36603479+vivekkumarshaw@users.noreply.github.com>
2018-08-24 04:57:44 -0400

    Add files via upload

---
+++ CA1st_930python.py
@@ -0,0 +1,26 @@
+
+first="Vivek"
+second="Kumar"
+last="Shaw"
+name1=first+" "+" "+second+" "+last
+print(name1)
+print("First Name=","Vivek")
+print("Second Name=","Kumar")
+print("Last Name=","Shaw")
+
+"reverse the name"
+
+print(name1[::-1])
+"last three character"
+
+print(name1[-3::1])
+
+"first 3 character"
+print(name1[0:3])
+
+"inbuild function"
+print(len(name1))
+
+
+
+
