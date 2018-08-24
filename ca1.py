b97f36d4888e7a84bcc040e38d7e7509cfbb3675
vivek kumar <36603479+vivekkumarshaw@users.noreply.github.com>
2018-08-24 04:39:33 -0400

    Add files via upload

---
+++ ca1.py
@@ -0,0 +1,90 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar']
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar shaw']
+Traceback (most recent call last):
+  File "D:/Python/CA1st_930.py", line 3, in <module>
+    concat(name)
+NameError: name 'concat' is not defined
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar shaw']
+First Name ['Vivek Kumar shaw']
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar shaw']
+First Name = []
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar shaw']
+First Name = ['Vivek Kumar shaw']
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar shaw']
+Traceback (most recent call last):
+  File "D:/Python/CA1st_930.py", line 3, in <module>
+    print("First Name =",name(4))
+TypeError: 'list' object is not callable
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar shaw']
+Traceback (most recent call last):
+  File "D:/Python/CA1st_930.py", line 3, in <module>
+    print("First Name =",name[0,4])
+TypeError: list indices must be integers or slices, not tuple
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar shaw']
+Traceback (most recent call last):
+  File "D:/Python/CA1st_930.py", line 3, in <module>
+    print[2]
+TypeError: 'builtin_function_or_method' object is not subscriptable
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar shaw']
+Traceback (most recent call last):
+  File "D:/Python/CA1st_930.py", line 3, in <module>
+    reverse(name)
+NameError: name 'reverse' is not defined
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar shaw']
+['Vivek Kumar shaw']
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar shaw']
+['Vivek Kumar shaw']
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek Kumar Shaw']
+
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek', 'Kumar', 'Shaw']
+
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek', 'Kumar', 'Shaw']
+
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek', 'Kumar', 'Shaw']
+Traceback (most recent call last):
+  File "D:/Python/CA1st_930.py", line 3, in <module>
+    print(name(0))
+TypeError: 'list' object is not callable
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek', 'Kumar', 'Shaw']
+First Name= Vivek
+>>> 
+====================== RESTART: D:/Python/CA1st_930.py ======================
+['Vivek', 'Kumar', 'Shaw']
+First Name= Vivek
+Second Name= Kumar
+Last Name= Shaw
+>>> 
